a3beeec2ff94c7fde6d9fbcf21c3c5a225f433ff
FIRST_NAME LAST_NAME Deepa <deepapandey364@gmail.com>
2021-11-07 16:34:17 +0530

    first class functions

--- practice.js
+++ practice.js
@@ -47,27 +47,49 @@
 // a();
 
 //SetTimeout examples:
-function x(){
-	for(var i = 1;i<=5;i++){ 
-		//It is happening bcz of closure which retains the value of it's lexical environment
-		// we've used var so when the loop runs first time it will make a copy of this func, attach a timer and point to the reference of i
-		//the 5 copies of this func will point to the same ref
-		// & will finally return the modified copy
-		//js will run in the bg & till then the value will be 6 which it will print 
-	setTimeout(
-	function(){
-		console.log(i);
-	},i*1000);
-}
-}
+// function x(){
+// 	for(var i = 1;i<=5;i++){ 
+// 		//It is happening bcz of closure which retains the value of it's lexical environment
+// 		// we've used var so when the loop runs first time it will make a copy of this func, attach a timer and point to the reference of i
+// 		//the 5 copies of this func will point to the same ref
+// 		// & will finally return the modified copy
+// 		//js will run in the bg & till then the value will be 6 which it will print 
+// 	setTimeout(
+// 	function(){
+// 		console.log(i);
+// 	},i*1000);
+// }
+// }
 //x();
 
-function y(){
-	for(let i = 1;i<=5;i++){ 
-	setTimeout(
-	function(){
-		console.log(i);
-	},i*1000);
+// function y(){
+// 	for(let i = 1;i<=5;i++){ 
+// 	setTimeout(
+// 	function(){
+// 		console.log(i);
+// 	},i*1000);
+// }
+// }
+// y();
+
+var x = function (param){
+  return function (){
+	console.log(param);
+    console.log("I am anonymous function assigned to x");
+  }
 }
+var y = function deepa (param){
+  return function deepak(){
+	console.log(param);
+	console.log(param());
+    console.log("I am anonymous function assigned to y");
+  }
 }
-y();
+function xyz(){
+  console.log("I am from function xyz");
+}
+x(xyz)();
+x(xyz())();
+y(xyz)();
+
+
